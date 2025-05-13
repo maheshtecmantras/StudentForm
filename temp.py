d308c9daa2f0d4dcd7778639e6a7e5d57c1ad211
hettecmantras <het.tecmantras@gmail.com>
2025-05-13 11:39:15 +0530

    Removed sensitive files and added to .gitignore

--- temp.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-# Replace this with your actual Apps Script deployment URL
-url = "https://script.google.com/macros/s/AKfycbz3UM6Zp50zXKO1d7S64n5F2pCw8q7tlB3bx-K-mk6_3Az_y9A-dTs-Nm0xTymdE5YltA/exec"
-response = requests.post(url)
-print(response.text)  # Will print the form edit URL
